7ed43374ce4de1a663a153804b688cac3bed6d29
Viviane Philipps <viviane.philipps@u-bordeaux.fr>
2020-03-30 14:40:02 +0000

    version 2.0.2

---
+++ R/dataEx.R
@@ -0,0 +1,17 @@
+#' Simulated dataset
+#' 
+#' Sample of 500 subjects simulated according to a linear mixed model. 
+#' The fixed part of the model included an intercept, 2 natural cubic splines on time 
+#' and their interactions with time-independent covariates $X1$, $X2$ and $X3$. 
+#' A random intercept and a independant error term were also inclued. 
+#' 
+#' @name dataEx
+#' @docType data
+#' @format A data frame with 2429 observations on the following 6 variables.
+#' \describe{ \item{i}{subject identification number}
+#' \item{t}{time of measurement} \item{X1}{binary covariate} 
+#' \item{X2}{continous Gaussian standard variable} 
+#' \item{X3}{continous Gaussian variable} 
+#' \item{Y}{longitudinal outcome}}
+#' @keywords datasets
+NULL
--- R/marqLevAlg-package.R
+++ R/marqLevAlg-package.R
@@ -1,4 +1,4 @@
-#' A parallelized algorithm for least-squares curve fitting.
+#' A parallelized general-purpose optimization based on Marquardt-Levenberg algorithm
 #'
 #' This algorithm provides a numerical solution to the problem of
 #' miniminzing/maximizing a function. This is more efficient than the
